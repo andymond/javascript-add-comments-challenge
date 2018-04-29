6133b5e0f9840228d489476c47ccf89a1914d6e3
Andrew Dymond <andy.dymond@gmail.com>
2018-04-29 14:49:37 -0600

    Add jquery to generate new comment form and add to comments list

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-  // add your code here
-})
+  $('#new_comment_button').click(function() {
+    const commentBody = '<textarea name="comment_body"></textarea>'
+    const commentAuthor = '<input name="author" type="text"></input>'
+    const submitComment = '<input id="comment-submit" name="submit" type="submit"></input>'
+    const commentForm = `<form id="new_comment">${commentBody}<br>${commentAuthor}${submitComment}</form>`
+    if ($('#comment-submit').length) {
+      console.log('form already present')
+    } else {
+      $(commentForm).appendTo("#comments")
+    }
+  });
+
+  $('#comments').on('click', '#comment-submit', function(event) {
+    event.preventDefault();
+    const body = $('textarea[name=comment_body]').val();
+    const author = $('input[name=author]').val();
+    const newComment = `<li>${body}<span class="author">${author}</span></li>`
+    if (body) {
+      $(newComment).appendTo('#comment_list');
+      $('#new_comment').remove();
+    };
+  });
+});
